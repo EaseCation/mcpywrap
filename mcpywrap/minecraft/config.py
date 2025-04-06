2105051793ba5665b47b5619fe69e4026d971e0a
boybook <fangyizhou1997@qq.com>
2025-04-06 23:42:20 +0800

    移除demo

--- mcpywrap/minecraft/config.py
+++
@@ -1,6 +0,0 @@
-ModName = "MyFirstMod"
-ModVersion = "1.0"
-ServerSystemName = "MyServerSystem"
-ServerSystemCls = "myScript.server.MyServerSystem.MyServerSystem"
-ClientSystemName = "MyClientSystem"
-ClientSystemCls = "myScript.client.MyClientSystem.MyClientSystem"
